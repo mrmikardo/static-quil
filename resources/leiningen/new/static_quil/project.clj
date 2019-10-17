5c7fa6519a2095bc824bd47aafb03c87490cb207
Jack Westmore <jack@withtab.com>
2019-10-17 22:56:20 +0100

    remove aot directive

--- resources/leiningen/new/static_quil/project.clj
+++ resources/leiningen/new/static_quil/project.clj
@@ -4,8 +4,9 @@
                  [quil "3.0.0"]
                  [org.apache.commons/commons-math3 "3.3"]]
   :jvm-opts ["-Xms1100m" "-Xmx1100M" "-server"] ; increase heap for large images
-  :java-source-paths ["src/java"]
-  :aot [{{name}}.core {{name}}.runcore])
+  :java-source-paths ["src/java"])
+
+; :aot [{{name}}.core {{name}}.runcore]) -> this just causes trouble!
 
 ; to generate large images, add ":main {{name}}.runcore" to the bottom of the
 ; project definition
