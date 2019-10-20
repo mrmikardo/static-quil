2c33b5082136f5468d9e73bc6a39eeb2ef16815c
Jack Westmore <jack@withtab.com>
2019-10-20 10:31:00 +0100

    Save and load last image-num from config

--- resources/leiningen/new/static_quil/core.clj
+++ resources/leiningen/new/static_quil/core.clj
@@ -4,17 +4,41 @@
             [clojure.java.shell :refer [sh]]))
 
 ; utility functions for scaling height and width, and for saving images
+; in sequence
+;
 ; based on: https://tylerxhobbs.com/essays/2015/using-quil-for-artwork
+;
 (defn h   ([] (h 1.0))   ([value] (* (q/height) value)))
 (defn w   ([] (w 1.0))   ([value] (* (q/width) value)))
 
-; relies on imagemagick command line utility
-(defn save-img [img-num]
-  (let [filename (str "output/sketch-" img-num ".tif")
-        thumb (str "output/sketch-" img-num "-1000.tif")]
+(defn parse-int [s]
+  (Integer. (re-find #"\d+" s)))
+
+(def img-nums (atom []))
+
+(defn load-imgnum-from-config []
+  (let [img-num (sh "cat" "img-num")]
+    (if (not (= (:exit img-num) 0)) 0 (parse-int (:out img-num)))))
+
+(defn load-imgnum-from-config-or-state []
+  (if (empty? @img-nums)
+    (do
+      (swap! img-nums conj (load-imgnum-from-config))
+      (last @img-nums))
+    (last @img-nums)))
+
+(defn save-latest-imgnum-to-config []
+  (sh "bash" "-c" (str "echo " (last @img-nums) " > img-num")))
+
+; relies on imagemagick command line utility ("convert")
+(defn save-img []
+  (let [img-num  (load-imgnum-from-config-or-state)
+        filename (str "output/sketch-" img-num ".tif")
+        thumb    (str "output/sketch-" img-num "-1000.tif")]
     (q/save filename)
     (sh "convert" "-LZW" filename filename)
     (sh "convert" "-scale" "1000x1000" filename thumb)
+    (swap! img-nums conj (inc img-num))
     (println "Saved image #" img-num)))
 
 ; end utility functions
