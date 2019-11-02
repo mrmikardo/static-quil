05f0867a85abf6ab8fefb3c371a1ff2e7679cdea
Jack Westmore <jack@withtab.com>
2019-11-02 12:28:17 +0000

    add data.generators

--- resources/leiningen/new/static_quil/project.clj
+++ resources/leiningen/new/static_quil/project.clj
@@ -1,6 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "My example sketch."
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.generators "0.1.2"]
                  [quil "3.0.0"]
                  [org.apache.commons/commons-math3 "3.3"]]
   :jvm-opts ["-Xms1100m" "-Xmx1100M" "-server"] ; increase heap for large images
